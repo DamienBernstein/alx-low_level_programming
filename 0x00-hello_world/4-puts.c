211adea47cea5b619f676b9e2c19cc5cf09aa14e
DamienBernstein <damienbernstein@gmail.com>
2022-09-08 04:12:51 -0700

    Task4.Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,2 @@
-#include <stdio.h>
-/**
-*description:main - prints out a string to stdout.
-*Return:0 if success
-*/
-int main(void)
-{
-    puts("\"programming is like building a multilingual puzzle");
-    return (0);
-}
+#!/bin/bash
+puts("\"Programming is like building a multilingual puzzle"); Return (0);
