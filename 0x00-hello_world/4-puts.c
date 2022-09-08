185bde68954874b84e307f96b55f9b5fa63b7447
DamienBernstein <damienbernstein@gmail.com>
2022-09-08 04:57:55 -0700

    Task4.hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main - prints out a string to a stdout
+ *Return: 0 if success
+ */
+int main(void)
+{
+puts("\"Programming is like building a mutilingual puzzle");
+return (0);
+}
