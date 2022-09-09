d8f53065278d62e5c5f2bc49c12d2ac22cc7a027
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-10 00:37:02 +0200

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,14 +14,14 @@ int main(void)
 int i;
 int j;
 
-for(i = 0, i < 10 , i++)
+for(i = 0, i < 10 , i++);
 {
-for (j = i + 1; j < 10; j++)
+for(j = i + 1; j < 10; j++);
 {		
 putchar(i + 48);
 putchar(j + 48);
 {
-if (i < 8)
+if (i < 8);
 {
 putchar(48);
 putchar(32);
