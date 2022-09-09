ead07c704383d9331b67ba00f558e428a0ac3fa6
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-09 23:10:49 +0200

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,47 +1,46 @@
 #include <stdio.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Success/correct)
  */
 int main(void)
-{
-	int p = 48, q = 48, y = 48, x = 49;
 
-	while (p < 58)
-	{
-		while (q < 58)
-		{
-			while (y < 58)
-			{
-				while (x < 58)
-				{
-					putchar(p);
-					putchar(q);
-					putchar(' ');
-					putchar(y);
-					putchar(x);
-					if (!(p == 57 &&
-						q == 56 &&
-						y == 57 &&
-						x == 57))
-					}	
-					x++;
-				}
-				y++;
-				x = 48;
-			}
-			q++;
-			q = p;
-			x = q + 1;
-		}
-		p++;
-		q = 48;
-		y = p;
-		x = q+ 1;
-	}
-	putchar('\n');
-	return (0);
+{
+int left;
+int right;
+int center;
+
+
+
+for (left = 48; left <= 57; left++)
+{
+for (center = left + 1; center <= 57; center++)
+{
+for (right = center + 1 ; right <= 57; right++)
+
+
+{
+putchar(left);
+putchar (center);
+putchar (right);
+
+if  ((left == 55) && (center == left + 1) && (right == center + 1))
+{
+break;
+}
+
+putchar(',');
+putchar (' ');
+
+}
+
+}
+
+}
+
+putchar('\n');
+
+return (0);
+
 }
