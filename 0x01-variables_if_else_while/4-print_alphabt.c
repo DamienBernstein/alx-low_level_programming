5831f326a1e39ec416840e84d28017b35da29faa
DamienBernstein <damienbernstein@gmail.com>
2022-09-09 03:43:06 -0700

    Task4.When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdion.h>
+
+/**
+ * main - program that prints the alphabet in lowercase, followed by a new line  
+ * you can only use puthchar 
+ * Return: 0
+ */
+
+int main(void)
+{
+int lw = 'a';
+while (lw <= 'z')
+{
+if (lw == 'e' || lw == 'q')
+{
+lw += 1;
+}
+else
+{
+putchar(lw);
+lw +=1;
+}
+}
+putchar('\n');
+return (0);
+} 
