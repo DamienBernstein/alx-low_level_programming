211edfcbcbe979ee5af06c4abf4fdb64674b4ee9
DamienBernstein <damienbernstein@gmail.com>
2022-09-13 09:36:18 -0700

    Task5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,36 +1,35 @@
-#include"main.h"
+#include <limits.h>
+
+#include "main.h"
+
+
+
 /**
-* print_sign - function that prints the sign of a number.
-* @n: The input number to check
-* Return: int.
-*/
 
-int print_sign(int n)
+ * main - check the code for Holberton School students.
+
+ *
+
+ * Return: Always 0.
+
+ */
+
+int main(void)
+  
 {
-
-if (n > 0)
-{
-_putchar(43);
-
-return (1);
-
-}
-
-else if (n < 0)
-
-{
-
-_putchar(45);
-
-return (-1);
-
-}
-
-else
+  
+int r;
+  
+r = print_sign(98);
+  
+if (r < 0)
     
-{
-
-_putchar(48);
-
+r *= -1;
+  
+_putchar(r + '0');
+  
+_putchar('\n');
+  
 return (0);
+  
 }
