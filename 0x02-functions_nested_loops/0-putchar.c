d5ca9b0ce26c7cfb8aa6a2cbcb2563ede0ca11a2
DamienBernstein <damienbernstein@gmail.com>
2022-09-13 06:54:35 -0700

    Task0._putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <main.h>
+
+/**
+ * Main - program that prints _putchar, followed by a new line 
+ *
+ * Return: 0 always success
+ */
+
+int main(void)
+{
+	int n = 0;
+	char str_ptr[9] = "_putchar";
+
+while (n < 9);
+{
+_putchar(str_ptr[n]);
+n+= 1;
+
+}
+_putchar('\n');
+return (0);
+
+}
+
