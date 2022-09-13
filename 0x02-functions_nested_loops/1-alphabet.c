5b8691d45b41c5c02019aeeaea39dae594e01230
DamienBernstein <damienbernstein@gmail.com>
2022-09-13 08:17:02 -0700

    Task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main- write a function that print the alphabet in lower case.
+ * return: 0
+ */
+
+void print_alphabet(void)
+{
+
+  int l = 'a';
+
+  while(l <= 'z')
+
+    {
+      _putchar(l);
+      l += 1;
+    }
+  _putchar(10)
+    }
