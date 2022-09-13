d943e41831931da2f365b69318e0b51465eebbee
DamienBernstein <damienbernstein@gmail.com>
2022-09-13 08:57:59 -0700

    Task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main- a function that prints the alphabet 10 times , in lowercase.
+ * you can only use _putchar twice in your code 
+ * return: 0 
+ */
+
+void print_alphabet_x10(void)
+
+  int l;
