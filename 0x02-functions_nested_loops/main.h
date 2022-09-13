6b6833b18345bddf79b136bbdb626964bf59fcba
DamienBernstein <damienbernstein@gmail.com>
2022-09-13 07:46:45 -0700

    Task0

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: on success 1.
+ * on error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(char c);
+
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
