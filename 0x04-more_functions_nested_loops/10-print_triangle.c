26f2d6d70dcff01872d5df48d715830410a7e0d5
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-15 18:37:19 +0200

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,11 +9,12 @@
 void print_triangle(int size)
 {
 int row, col, aux;
+
 if (size > 0)
 {
 for (row = 0; row < size; row++)
 {
-for(col = 0; col < size; col++)
+for (col = 0; col < size; col++)
 {
 aux = (size - row) - 1;
 if (col < aux)
