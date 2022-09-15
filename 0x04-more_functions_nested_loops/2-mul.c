8ffaee382311d49f109e6159710625c6c2184a59
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-15 15:09:14 +0200

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-*mul -function that multiplies two intergers.
-*@a: the first input value to chec
-*@b: the second input value to check
-*Return: int.
-*/
+ * mul -function that multiplies two intergers.
+ * @a: the first input value to chec
+ * @b: the second input value to check
+ * Return: int.
+ */
 
 int mul(int a, int b)
 {
