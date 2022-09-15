9f6d6caf8510fbe9e10d72a8606f8c8719f41782
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-15 15:25:45 +0200

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,12 +8,12 @@
 
 void print_numbers(void)
 {
-	int n = 0;
+int n = 0;
 
-	while (n < 10)
-	{
-		_putchar(n + '0');
-		n += 1;
-	}
-	_putchar('\n');
+while (n < 10)
+{
+_putchar(n + '0');
+n += 1;
+}
+_putchar('\n');
 }
