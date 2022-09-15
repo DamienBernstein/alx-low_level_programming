5609467413fb3937ed5a3db5a3065e2afeedebe1
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-15 14:13:20 +0200

    Update 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - this function checks for upper case letters.
+ * @c: input value to check 
+ * Return: 1 if c is upper case, or 0 if found none.
+ */
+ 
+ int _isupper(int c)
+ {
+ if (c >= 'A' && c >= 'Z')
+ return (1);
+ {
+ else
+ {
+ return (0);
+ }
