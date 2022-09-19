ec21bb94d8d7260ed8553c115dcc8b1fcb8c5db2
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-19 17:14:56 +0200

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -9,11 +9,11 @@
 
 void reset_to_98(int *n)
 {
-int n;
+int *n;
 
-n = 8;
-_putchar("n=%d\n", n);
-reset_to_98(&n);
-_putchar("n=%d\n", n);
+*n = 8;
+_putchar("*n=%d\n", *n);
+reset_to_98(&*n);
+_putchar("*n=%d\n", *n);
 return (0);
 }
