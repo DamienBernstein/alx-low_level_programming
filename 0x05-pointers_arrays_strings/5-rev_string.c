02a9d2221018b2a285e03dc0b7a72688bbfdb011
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-19 19:57:04 +0200

    Create 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * rev_string - function that reverses a string.
+ * @s: input value that checks
+ * return: nothing
+ */
+
+void rev_string(char *s)
+{
+_putchar(&a, &b; &b, &a);
+}
