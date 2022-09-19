a257ae0ece511954ec524b31a6b55ac1afe9426e
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-19 17:27:34 +0200

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,18 +3,15 @@
 
 /**
  * swap - function that swaps the values of two integers
- * return: int.
+ * @a, b: input value to check
+ * return: nothing
  */
 
 void swap_int(int *a, int *b)
 {
-int a;
-int b;
-
-a = 42;
-b = 98;
-_putchar("a=%d, b=%d\n", a, b);
-swap_int(&a, &b);
-_putchar("a=%d, b=%d\n", a, b);
-return (0);
+*a = 42;
+*b = 98;
+_putchar("a=%d, b=%d\n", *a, *b);
+swap_int(&*a, &*b);
+_putchar("a=%d, b=%d\n", *a, *b);
 }
