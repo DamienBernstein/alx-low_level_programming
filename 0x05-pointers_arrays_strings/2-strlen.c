bee747b50b8496e8f6c8483f7ec94de7d14c45ca
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-19 17:51:25 +0200

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * strlen - function that returns the length of a string.
+ * _strlen - function that returns the length of a string.
  * @s: input value to check.
  * return: int.
  */
