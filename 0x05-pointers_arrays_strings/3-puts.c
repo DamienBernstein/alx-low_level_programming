4c6b4ad683ac7d25a1cd41319ffed9ac6b84040f
DamienBernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-19 18:00:57 +0200

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout.
+ * @str: input value to check
+ * Return: nothing 
+ */
+
+void _puts(char *str)
+{
+int i;
+
+while(str[i] != '\0')
+{
+_putchar(str[i]);
+i += 1;
+}
+_putchar('\n');
+}
