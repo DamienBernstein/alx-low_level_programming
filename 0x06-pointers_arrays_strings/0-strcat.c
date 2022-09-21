07a8c9c545d06a1e369e9ccbd08361fcf3b0dd6a
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-21 18:25:10 +0200

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,8 +12,8 @@ char *_strcat(char *dest, char *src)
 {
 unsigned int i = 0;
 unsigned int j = 0;
-  
-while (*(dest + i) != '\0') 
+
+while (*(dest + i) != '\0')
 i++;
 while (*(src + j) != '\0')
 {
