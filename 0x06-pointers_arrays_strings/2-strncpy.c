cb6ea9359d858f7d45abf4b36a29546bdc2f5f76
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-21 14:13:13 +0200

    Create 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,11 @@
+#include "main.h" 
+
+/**
+ * _strncpy - function that copies a string
+ * @src: input value 
+ * @dest: input value
+ * @n: input value 
+ * return:
+ */
+
+char *_strncpy(char *dest, char *src, int n)
