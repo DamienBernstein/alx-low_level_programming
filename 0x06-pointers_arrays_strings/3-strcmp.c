41fed43ac85c075039e72b36443fd769c1e5c46f
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-21 20:51:24 +0200

    Create 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strcmp-function that compares two strings.
+ * @dest: input value that will be compared.
+ * @src: input value that will be compared.
+ * return: int
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+int size_t n;
+int size_t n(char *s1);
+strncmp("char *s1 == char *s2\n");
+return (0);
+}
+ 
