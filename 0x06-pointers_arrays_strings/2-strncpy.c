96a53e7143a4de739c85babb7c1074d11e29aa54
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-21 19:59:09 +0200

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,12 +5,12 @@
  * @src: string that appends to dest.
  * @dest: input value that is to be copied of the destination.
  * @n: number of bytes to allocate.
- * return: string.
+ * Return: return. 
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-int n;
+int i;
 for (i = 0; i < n && src[i] != '\0'; i++)
 dest[i] = src[i];
 for (; i < n; i++)
