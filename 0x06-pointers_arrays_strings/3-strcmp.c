f0559d1a96aae9abc44f8138d3d04f0a8a41a9f8
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-21 20:54:07 +0200

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,14 +4,15 @@
  * _strcmp-function that compares two strings.
  * @dest: input value that will be compared.
  * @src: input value that will be compared.
- * return: int
+ * return: int.
  */
 
 int _strcmp(char *s1, char *s2)
 {
+char *s1 = "dest";
+char *s2 = "src";
 int size_t n;
 int size_t n(char *s1);
 strncmp("char *s1 == char *s2\n");
 return (0);
 }
- 
