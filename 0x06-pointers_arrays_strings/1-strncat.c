36afac68ec69472925c5713d925c12cc72332c5c
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-21 13:02:55 +0200

    Update 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strncat - function that concatenates two strings.
+ * @dest: input value that checks
+ * @src: input value that checks
+ * @n: input value that checks
+ * Return: int 
+ */
+ 
+char *_strncat(char *dest, char *src, int n)
+{
+char s1[100] = "dest";
+char s2[] = "src";
+int length, n;
+
+length = 0;
+while(s1[length] != '\0') {
+++length;
+}
+for (n = 0; s2[n] != '\0');
+length++;
+n++;
+}
+if (i = 0; src >= n; i++; length++) {
+continue;
+} else {
+s1[length] = '\0';
+}
+_putchar("dest");
+_putchar("s1");
+_putchar('\n');
+return (0);
+}
