27de11bc0a1b72b688160b6754b6f1998597468b
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-22 10:30:52 +0200

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,12 +12,14 @@ void reverse_array(int *a, int n)
 int *a[n];
 int temp;
 
-for (i = 0; i < n/2; i++) {
+for (i = 0; i < n /2; i++)
+{
 temp = arr[i];
 arr[i] = arr[n - i - 1];
 arr[n - i - 1] = temp;
 }
-for (int i = 0; i < n; i++) {
+for (int i = 0; i < n; i++)
+{
 _putchar("%d; arr[i]");
 }
 }
