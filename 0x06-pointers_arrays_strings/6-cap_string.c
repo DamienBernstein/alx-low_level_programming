d0c2cb5681fc422011cc866850fca054f42c11d8
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-22 14:54:56 +0200

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * cap_string - function that capitalizes the
- * first character of words in a string.
- * @str: string to capitalize
- * Return: returns capitalized string.
- */
+*cap_string - function that capitalize first character of a word
+*@str: string to capitalize
+*Return:returns the capitalized string
+*/
 
 char *cap_string(char *str)
 {
@@ -16,24 +15,20 @@ while (str[++index])
 while (!(str[index] >= 'a' && str[index] <= 'z'))
 index++;
 
-if (str[index - 1] ==  ' ' ||
+if (str[index - 1] == ' ' ||
 str[index - 1] == '\t' ||
 str[index - 1] == '\n' ||
 str[index - 1] == ',' ||
 str[index - 1] == ';' ||
 str[index - 1] == '.' ||
 str[index - 1] == '!' ||
+str[index - 1] == '?' ||
 str[index - 1] == '"' ||
 str[index - 1] == '(' ||
 str[index - 1] == ')' ||
 str[index - 1] == '{' ||
-str[index - 1] == '}' ||
-str[index - 1] == ' ' ||
+str[index - 1] == '}')
 str[index] -= 32;
-}    
-else 
-i++;
-}    
 }
 return (str);
 }
