c46c24e97364bcaf23ad4f81f2acc29b48d8f7f3
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-22 11:34:32 +0200

    Create 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * string_toupper - function that changes all lowercase letters of a string to uppercase.
+ * @char: 
+ * return: 
+ */
+
+char *string_toupper(char *)
+{
+int i = 0)
+  while(*(ch + i) != '\0')
+  {
+    if (*(ch + i) >= 97 && *(ch + i) <= 255)
+      (*(ch +i) = (*(ch +i) - *(ch 
