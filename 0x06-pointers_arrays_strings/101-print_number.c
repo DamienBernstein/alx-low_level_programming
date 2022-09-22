88caf4cc2c0e0de52ff93f628648fa71e228c020
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-22 16:15:06 +0200

    Create 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/** 
+ * print_number - function that prints an integer.
+ * @n: input value that checks.
+ * Return: int
+ */
+
+void print_number(int n)
+{
+_putchar("n");
+_putchar('\n');
+}
