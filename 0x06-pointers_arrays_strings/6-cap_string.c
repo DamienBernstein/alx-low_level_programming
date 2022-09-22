addc197a6c88b1d1a59094cf404fcb64dd237a7d
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-22 14:59:01 +0200

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -27,8 +27,7 @@ str[index - 1] == '"' ||
 str[index - 1] == '(' ||
 str[index - 1] == ')' ||
 str[index - 1] == '{' ||
-str[index - 1] == '}' ||
-str[index - 1] == ' ' )
+str[index - 1] == '}' )
 str[index] -= 32;
 }
 return (str);
