364ee9a54358d5649bcb2a2d8f52ae9f85f0470d
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-22 10:29:04 +0200

    Create 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_array-function that reverses the content of an array of integers.
+ * @n: number of elements to swap.
+ * @a: array of intergers.
+ * Return: nothing
+ */
+
+void reverse_array(int *a, int n)
+{
+int *a[n];
+int temp;
+
+for (i = 0; i < n/2; i++) {
+temp = arr[i];
+arr[i] = arr[n - i - 1];
+arr[n - i - 1] = temp;
+}
+for (int i = 0; i < n; i++) {
+_putchar("%d; arr[i]");
+}
+}
