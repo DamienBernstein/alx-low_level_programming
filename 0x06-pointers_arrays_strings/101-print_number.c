29dffc17fa96446c171d79f9993078d545d6a5dc
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-22 23:23:18 +0200

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,32 +6,19 @@
  *
  * Return: Null Void
  */
+
 void print_number(int n)
 {
-	unsigned int m, d, count;
+unsigned int integer = n;
 
-	if (n < 0)
-	{
-		_putchar(45);
-		m = n * -1;
-	}
-	else
-	{
-		m = n;
-	}
-
-
-	d = m;
-	count = 1;
-
-	while (d > 9)
-	{
-		d /= 10;
-		count *= 10;
-	}
-
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((m / count) % 10) + 48);
-	}
+if (n < 0)
+{
+_putchar('-');
+integer = -integer;
+}
+if ((integer / 10) > 0)
+{
+print_number(integer / 10);
+}
+_putchar((integer % 10) + 48);
 }
