6e6bf624d3b0055a5eea681a3e0344c300b9955e
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-22 11:16:56 +0200

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,8 +2,8 @@
 
 /**
  * reverse_array - function that reverses the content of an array of integers.
- * @n: number of elements to swap.
- * @a: array of intergers.
+ * @a: pointer of an array int.
+ * @n: The number of elements to swap.
  * Return: nothing
  */
 
@@ -20,7 +20,6 @@ n -= 1;
 while (i < n)
 i += 1;
 while (j <= i)
-j += 1;
 {
 aux = *(str + j);
 *(str + j) = *(array + i);
