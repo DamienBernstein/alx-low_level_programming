6dd15a4f31030566c45bf2fad095b3bbc1fb6a76
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-22 15:38:57 +0200

    Create 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _leet - function that that encodes a string into 1337.
+ * @s: pointer to char.
+ * Return: char.
+ */
+
+char *leet(char *s)
+{
+int i = 0;
+int j = 0;
+char *l = "aAeEoOlLtT";
+char *n = "4433001177";
+
+while (*(s + i) != '\0')
+{
+while (*(j + l) != '\0')
+{
+if (*(s + i) == *(l + j))
+*(s + i) = *( j + n);
+j++;
+}
+j = 0;
+i++;
+}
+return (s);
+}
