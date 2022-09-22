c058ae6614ddc39ed33a1eb2941fa7afec31f5aa
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-22 11:22:07 +0200

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,13 +12,15 @@ int _strcmp(char *s1, char *s2)
 {
 int i = 0;
 int aux;
+
 while (*(s1 + i) != '\0')
 {
 if (*(s1 + i) > *(s2 + i))
+{
 aux = *(s1 + i) - *(s2 + i);
 return (aux);
 }
-else if *(s1 + i) < *(s2 + i)
+else if (*(s1 + i) < *(s2 + i))
 {
 aux = *(s1 + i) - *(s2 + i);
 return (aux);
