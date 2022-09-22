51be3a61268bb7a472942d773c1352e96ea9b2d0
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-22 16:35:07 +0200

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,20 +7,8 @@
 
 void print_number(int n)
 {
-unsigned int n1;
-
-if (n < 0)
-{
-n1 = -n;
-_putchar('-');
+write ("n");
+if (n = 0; n < 0; n++);
 }
-else
-{
-n1 = n;
-}
-
-if (n1 / 10)
-print_number(n1 / 10);
-
-_putchar((n1 % 10) + '0');
+putchar('\n');
 }
