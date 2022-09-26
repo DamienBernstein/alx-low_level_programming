26d6771932f9172b954db1d2c5a330874f032177
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-26 17:04:33 +0200

    Create 5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strstr - function that locates a substring
+ * @haystack: string
+ * @needle: substring
+ * Return: pointer to the beginning of the located substring,
+ * or NULL if the substring is not found.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+char *haystack = [];
+char *needle = [];
+char *result;
+
+result = strstr(string1,string2);
+_putchar {"%s/n, result");
+}
