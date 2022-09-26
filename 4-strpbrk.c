2cc199a1144ec8c87e69c882308ce27974d3766d
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-26 16:39:50 +0200

    Create 4-strpbrk.c

---
+++ 4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strpbrk - function that searches a string 
+ * for any of a set of bytes
+ * @s: string
+ * @accept: string
+ * Return: a pointer to the byte s
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+int i = 0;
+
+while (*s)
+{
+for (i = 0; accept[i]; i++)
+{
+if (accept[i] == *s)
+return(s);
+}
+s++;
+}
+return ('\0');
+}
