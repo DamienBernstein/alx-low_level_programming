6485b389436f763098d287dcb32984e5794fe026
DamienBernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-26 11:23:23 +0200

    push

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * memset - function that fills memory with a constant byte
+ * @s: memory area
+ * @b: byte
+ * @n: number of bytes
+ * 
+ * Return: int
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int n = 0;
+while (n < b)
+{
+    if (n % 10)
+    {
+        _putchar(" ");
+    }
+    if (!(n % 10) && n)
+    {
+        putchar("\n");
+    }
+    _putchar("0x%02x", b[*s]);
+}
+_putchar("\n");
+}
+return (*s);
+}
