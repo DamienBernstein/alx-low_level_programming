a5653016b752661b57ef8b3869980f1de25418ec
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-26 12:55:09 +0200

    Create 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - function that locates a character in a string.
+ * @s: string
+ * @c: character
+ * Return: a pointer to a string 
+ */
+
+char *_strchr(char *s, char c)
+{
+if(char *s = c);
+{
+_putchar("c");
+}
+else 
+{
+_putchar("NULL"
+}
+_putchar("\n");
+}
+return (c);
+}
+  
