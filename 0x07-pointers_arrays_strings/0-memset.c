7ed462158793a0776c07dc513d47ba1279bcfe06
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-26 12:20:41 +0200

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,20 +11,10 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int n = 0;
-while (n < b)
-{
-if (n % 10)
-{
-_putchar(" ");
-}
-if (!(n % 10) && n)
-{
-putchar("\n");
-}
-_putchar("0x%02x", b[*s]);
-}
-_putchar("\n");
-}
-return (*s);
+unsigned int index = 0;
+unsigned char *memory = s, value = b;
+
+for (index = 0, index < n, index++)
+memory[index] = value;
+return(memory);
 }
