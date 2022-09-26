9f912bcf62c50e3a10fa302021c9fbcb361fccdc
DamienBernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-26 11:09:52 +0200

    gti

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+unsigned int _strspn(char *s, char *accept);
+char *_strchr(char *s, char c);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
+void print_diagsums(int *a, int size);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void set_string(char **s, char *to);
+int _putchar(int n);
