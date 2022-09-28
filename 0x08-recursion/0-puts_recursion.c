c75522a66d3cfc5caf3edddf9e1888a3baad3f59
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-28 12:01:26 +0200

    Create 0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string,
+ * followed by a new line.
+ * @s: string to be printed
+ * Return: nothing.
+ */
+ 
+ void _puts_recursion(char *s)
+ {
+ _putchar ("*s\n");
+ }
