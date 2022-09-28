f3c8cf38eca89cea3d31f76b2581bea43ed170ae
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-28 12:40:48 +0200

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,11 +1,19 @@
 #include "main.h"
 
 /**
- * strlen_recursion.c - function that prints the string in reverse.
+ * strlen_recursion.c - function that prints the
+ * length of the string
  * @s: string 
  * Return: nothing
  */
 
-void _print_rev_recursion(char *s)
+int _strlen_recursion(char *s)
 {
-  char *s[]
+if (*s != '\0')
+{
+_strlen_recurion(*s)
+_putchar(s);
+}
+else
+return;
+}
