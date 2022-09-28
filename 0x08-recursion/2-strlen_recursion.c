67b3bf71b224f011e762333cffd553e61128f617
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-28 12:44:05 +0200

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,11 +9,8 @@
 
 int _strlen_recursion(char *s)
 {
-if (*s != '\0')
-{
-_strlen_recursion(*s);
-_putchar(s);
-}
-else
-return;
+if (*s == '\0')
+return (0);
+s++;
+return (1 + _strlen_recursion(s));
 }
