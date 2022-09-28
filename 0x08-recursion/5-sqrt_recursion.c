804ae7ec53642a83144a2696eb6624c24d6968ea
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-28 13:13:04 +0200

    Create 5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _sqrt_recusion - function the natural square root of a number
+ * @n: number to be square rooted
+ * Return: int
+ */
+
+int _sqrt_recursion(int n)
+{
+if (n == 0)
+return (1);
+else
+return (-1);
+return (n * _sqrt_recursion(n - 1);
+}
