65e8c9b28c111636fb51b511156d1377fd1661c9
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-30 15:52:54 +0200

    Update 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program that prints its name
+ * @argc: arguement count
+ * @argv: arguement vector
+ * Return: int
+ */
+ 
+ int main(int argc, char *argv[])
+ {
+ int count;
+ 
+ for (count = 0; count < argc; count++)
+ }
+ if (argcv == 1)
+ printf("%s", argv[count]);
+ else
+ break;
+ }
+ printf('\n');
+ }
+ return (0);
+ }
+ 
