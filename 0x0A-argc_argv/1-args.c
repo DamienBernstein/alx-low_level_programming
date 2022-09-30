1a86a5571d0ca4f613b168385ea2c15cb8ce188f
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-30 16:39:34 +0200

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - prints number of arguements
- * @argc: number of arguements
- * @argv: array of arguements
+ * main - prints the number of arguments
+ * @argc: number of arguments
+ * @argv:array of arguments
  * Return: the number of arguments
  */
 
-int main(int argc, char __attribute__((__unused)) *argv[])
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
 printf("%d\n", argc - 1);
+
 return (0);
 }
