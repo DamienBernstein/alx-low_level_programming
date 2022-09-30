bbce278230c4fb280d48f6ac297e80cd272b918f
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-09-30 16:38:41 +0200

    Create 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguements
+ * @argc: number of arguements
+ * @argv: array of arguements
+ * Return: the number of arguments
+ */
+
+int main(int argc, char __attribute__((__unused)) *argv[])
+{
+printf("%d\n", argc - 1);
+return (0);
+}
