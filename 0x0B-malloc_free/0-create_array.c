33f993a8be3f25d0a2710844ea733c9798fbcf14
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-10-03 15:27:43 +0200

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,12 +3,25 @@
 /**
  * create_array - function that creates an array of chars,
  * and initializes it with a specific char
- *
- *
+ * @size: size of array
+ * @c: special character
  * Returns: pointer to an array, or NULL if it fails
  */
- 
- char *create_array(unsigned int size, char c)
- {
- 
- 
+
+char *create_array(unsigned int size, char c)
+{
+char *array;
+unsigned int index;
+
+if (size == 0)
+return (NULL);
+array = malloc(sizeof(char) * size);
+
+if (array == NULL)
+return (NULL);
+
+for (index = 0; index < size; index++)
+array[index] = c;
+
+return (array);
+}
