d7cd90a51ce15fac7dc663e63ae5421a4ffa6801
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-10-03 12:55:38 +0200

    Update 0-create_array.c

---
+++ 0x08-malloc_free/0-create_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * create_array - function that creates an array of chars,
+ * and initializes it with a specific char
+ *
+ *
+ * Returns: pointer to an array, or NULL if it fails
+ */
+ 
+ char *create_array(unsigned int size, char c)
+ {
+ 
+ 
