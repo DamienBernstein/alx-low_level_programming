78cac51a569ee22001262259adfa007652279729
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-10-03 19:10:55 +0200

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,6 +10,28 @@
 
 char *str_concat(char *s1, char *s2)
 {
-  char *concat_str;
-  int index, concat_index
-  
+char *concat_str;
+int index, concat_index = 0, len = 0;
+
+if (s1 == NULL)
+return = "";
+
+if (s2 == NULL)
+return = "";
+
+for (index = 0; s1[index] || s2[index]; index++)
+len++;
+
+concat_str = malloc(sizeof(char) * (len);
+
+if (concat_str == NULL)
+return (NULL);
+
+for (index = 0; s1[index]; index++)
+concat_str[concat_index++] = s1[index];
+
+for (index = 0; s2[index]; index++)
+concat_str[concat_index++] = s2[index];
+
+return (concat_str);
+}
