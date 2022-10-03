bed1c29f43e0ceb2ec9fe6ffd38ee41099f62f63
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-10-03 16:28:42 +0200

    Create 2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * str_concat - function that concatenates two strings.
+ * @s1: string one
+ * @s2: string two
+ *
+ * Return: NULL on failure
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+  char *concat_str;
+  int index, concat_index
+  
