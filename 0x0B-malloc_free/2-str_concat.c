24f6f3364f59b7b12dbca11b3202c20e337f64ff
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-10-03 19:17:18 +0200

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,28 +1,29 @@
 #include "main.h"
 
 /**
- * str_concat - function that concatenates two strings.
- * @s1: string one
- * @s2: string two
- *
- * Return: NULL on failure
- */
+* str_concat - a function that concatenates two strings.
+*@s1:First string
+*@s2:Second string
+*
+*Return: NULL in case of failure , but pointer to new string in
+*case of success
+*/
 
 char *str_concat(char *s1, char *s2)
 {
 char *concat_str;
-int index, concat_index = 0, len = 0;
+int index, concat_index = 0,  len = 0;
 
 if (s1 == NULL)
-return = "";
+s1 = "";
 
 if (s2 == NULL)
-return = "";
+s2 = "";
 
 for (index = 0; s1[index] || s2[index]; index++)
 len++;
 
-concat_str = malloc(sizeof(char) * (len);
+concat_str = malloc(sizeof(char) * len);
 
 if (concat_str == NULL)
 return (NULL);
