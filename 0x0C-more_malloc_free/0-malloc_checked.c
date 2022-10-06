aa11f280f8baa890bd861939dfa42f5741ff5d78
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-10-06 16:30:09 +0200

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,10 +2,19 @@
 
 /**
  * malloc_cheked - function that allocates memory using malloc
- * @b:
- *
+ * @b: number of memory
  * Return: nothing
  */
- 
+
 void *malloc_checked(unsigned int b)
 {
+void *p;
+
+p = malloc(b);
+if (p == NULL)
+{
+exit(98);
+}
+return (p);
+}
+ 
