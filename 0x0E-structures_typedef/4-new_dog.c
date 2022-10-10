996df7b3321926361e75aa7572aa93cde1deda76
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-10-10 14:03:08 +0200

    Create 4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,76 @@
+#include "dog.h"
+
+/**
+ * _strlen - returns the length of the string
+ * @str: string to be counted
+ * return: the length of the string
+ */
+
+int _strlen(char *str)
+{
+int len = 0;
+while (str)
+len++;
+
+return(len);
+}
+
+/**
+ * _strcpy - copy string pointed by src
+ * @src: buffer storing string to copy
+ * @dest: buffer storing the string
+ * return: copied string
+ */
+
+char *_strcopy(char *dest, char *src)
+{
+int index = 0;
+  
+for (; src[index]; index++)
+dest[index] = src[index];
+  
+dest[index] = '\0';
+return (dest);
+}
+
+/**
+*new_dog - creates a new dog
+*@name: name of new dog
+*@age: age of new dog
+*@owner: owner of new dog
+*Return: returns NULL in case
+*of failure
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *doggo
+
+if (name == NULL || age < 0 || owner == NULL)
+return (NULL);
+
+doggo = malloc(sizeof(dog_t));
+if (doggo == NULL)
+return (NULL);
+
+doggo->name = malloc(sizeof(char) * (strlen(name) + 1));
+if (doggo ->name == NULL)
+{
+free(doggo);
+return (NULL);
+}
+
+doggo->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+if (doggo == NULL)
+{
+free(doggo->name);
+free(doggo);
+return (NULL);
+}
+
+doggo->name = _strcopy(doggo->name, name);
+doggo->age = age;
+doggo->owner = _strcopy(doggo->owner, owner);
+
+return (doggo);
+}
