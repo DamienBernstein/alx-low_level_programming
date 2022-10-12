0543caf524ba4368c51856075bcdc96a4f862c4b
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-10-12 08:08:43 +0200

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,15 +2,15 @@
 
 /**
  * print_name - function that prints a name
- * @name: name tahe will be printed
- * @f: pointer to function
+ * @name: name that will be printed
+ * @f: function which prints name
  *  Return: nothing
  */
 
 void print_name(char *name, void (*f)(char *))
 {
+if (name == NULL || f == NULL)
+return;
 
-
-
-    
+f(name);
 }
