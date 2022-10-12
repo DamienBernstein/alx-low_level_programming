35d82fc908aec48fcd5d41680fd6f35f048d0f43
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-10-12 14:02:50 +0200

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,12 +1,7 @@
-#ifndef FUNCTION_POINTERS.H
-#define FUNCTION_POINTRS.H
-
-#include <stdlib.h>
-#include <stdio.h>
-
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+typedef __SIZE_TYPE__ size;
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-int (*get_op_func(char *s))(int, int);
-
-#endif /*MAIN_H*/
+#endif /* #ifndef MAIN_H */
