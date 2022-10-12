ce5a1b009b361a85ea7e3a4fa77a1a5dea3e92e8
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-10-12 08:11:19 +0200

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,12 @@
-#ifndef
-#define
+#ifndef FUNCTION_POINTERS.H
+#define FUNCTION_POINTRS.H
+
+#include <stdlib.h>
+#include <stdio.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int (*get_op_func(char *s))(int, int);
 
-#endif
+#endif /*MAIN_H*/
