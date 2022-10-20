c6aff3dc5846c023a407f93c154c7e3eb6924b25
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-10-20 16:16:36 +0200

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,27 +13,27 @@ list_t *add_node(list_t **head, const char *str)
 {
 	char *dup;
 	int len;
-	list_t *new;
+	list_t *new_node;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
 		return (NULL);
 
 	dup = strdup(str);
 	if (dup == NULL)
 	{
-		free(new);
+		free(new_node);
 		return (NULL);
 	}
 	for (len = 0; str[len];)
 		len++;
 
-	new->str = dup;
-	new->len = len;
-	new->next = *head;
+	new_node->str = dup;
+	new_node->len = len;
+	new_node->next = *head;
 
-	*head = new;
+	*head = new_node;
 
-	return (new);
+	return (new_node);
 
 }
