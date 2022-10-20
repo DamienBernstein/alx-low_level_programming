a533ca064a3d10d2479d0b4e2b0b1b25f97ee8e7
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-10-20 13:21:08 +0200

    Update 0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @str: string
+ * @h: head
+ * Return: number of nodes
+ */
+ 
+ size_t print_list(const list_t *h)
+ {
+ char *str;
+ 
+ if (str == NULL)
+ printf([0], nil);
+ 
+ 
+ return (0);
+ }
+ }
