2fa5cff169231b2bd09a7c7949df82e57c4560b3
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-10-20 16:13:59 +0200

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -13,34 +13,33 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	char *dup;
 	int len;
-	list_t *new, *last;
+	list_t *new_node, *last_node;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
 		return (NULL);
 
 	dup = strdup(str);
 	if (str == NULL)
 	{
-		free(new);
+		free(new_node);
 		return (NULL);
 	}
 
-	for (len = 0; str[len];)
-		len++;
+	for (len = 0; str[len]; len++)
 
-	new->str = dup;
-	new->len = len;
-	new->next = NULL;
+	new_node->str = dup;
+	new_node->len = len;
+	new_node->next = NULL;
 
 	if (*head == NULL)
-		*head = new;
+		*head = new_node;
 	else
 	{
-		last = *head;
-		while (last->next != NULL)
-			last = last->next;
-		last->next = new;
+		last_node = *head;
+		while (last_node->next != NULL)
+			last_node = last_node->next;
+		last_node->next = new_node;
 	}
 	return (*head);
 }
