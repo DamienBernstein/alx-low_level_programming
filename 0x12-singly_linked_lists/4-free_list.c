4710e0153b7712ee40e1a2e99d22fd2c84e1d40f
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-10-20 15:55:18 +0200

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -12,7 +12,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new_node, *last;
 	char *dup;
 		int len;
-	
+
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
@@ -22,14 +22,14 @@ list_t *add_node_end(list_t **head, const char *str)
 		free(new_node);
 		return (NULL);
 	}
-	
+
 	for (len = 0; str[len])
 		len++;
-	
+
 	new_node->str = dup;
 	new_node->len = len;
 	new_node->next = NULL;
-	
+
 	if (*head == NULL)
 		*head = new_node;
 	else
