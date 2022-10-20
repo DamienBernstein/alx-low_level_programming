142d7cfbc2e648a40750f763469792ea134bc79e
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-10-20 15:25:55 +0200

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,17 +7,16 @@
  * Return: address of the new element, or NULL if it failed
  */
  
- list_t *add_node(list_t **head, const char *str)
- {
- list_t *new_node;
- 
- new_node = malloc(sizeof(list_t));
- if (new_node == NULL)
- return (NULL);
- new_node->len = _strlen(str);
- new_node->str = strdup(str);
- new_node->next = *head;
- *head = new_node;
- return (new_node);
- }
- 
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *new_node;
+
+new_node = malloc(sizeof(list_t));
+if (new_node == NULL)
+return (NULL);
+new_node->len = _strlen(str);
+new_node->str = strdup(str);
+new_node->next = *head;
+*head = new_node;
+return (new_node);
+}
