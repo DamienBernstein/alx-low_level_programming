d1e98ad2b02dda43d87055cc4d889e2fbfd672bb
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-10-20 15:59:08 +0200

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,44 +1,46 @@
 #include "lists.h"
 
 /**
- * add_node_end - function that adds a new node at the end of a list_t list
- * @head: input header pointer
- * @str: Input string value
- * Return: the address of the new element, or NULL if it failed
- */
+ * add_node_end - Adds a new node at the end of a list
+ *@head: pointer to the head of the list
+ *@str: string to be added to the list
+ *
+ *Return: NULL in case of failure
+ *or address or the element
+*/
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node, *last;
 	char *dup;
 	int len;
+	list_t *new, *last;
 
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 		return (NULL);
 
 	dup = strdup(str);
-	if (str == null)
+	if (str == NULL)
 	{
-		free(new_node);
+		free(new);
 		return (NULL);
 	}
 
-	for (len = 0; str[len])
+	for (len = 0; str[len];)
 		len++;
 
-	new_node->str = dup;
-	new_node->len = len;
-	new_node->next = NULL;
+	new->str = dup;
+	new->len = len;
+	new->next = NULL;
 
 	if (*head == NULL)
-		*head = new_node;
+		*head = new;
 	else
 	{
 		last = *head;
 		while (last->next != NULL)
-		last = last->next;
-		last->next = new_node;
+			last = last->next;
+		last->next = new;
 	}
 	return (*head);
 }
