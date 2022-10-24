2d457262ebc772e7ca7e4ba6b00fde0f8e26d9d0
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-10-24 20:01:18 +0200

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,10 +8,10 @@
  * Returns: the address of the new node, or NULL if it failed
  */
 
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 {
 	listint_t *temp;
-	listin_t *new_node;
+	listint_t *new_node;
 	unsigned int i;
 
 	new_node = malloc(sizeof(listint_t));
