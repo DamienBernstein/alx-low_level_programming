944a4e0ab6167d8ffd4ca80d0411672b46329b1d
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-10-24 13:25:36 +0200

    Update 3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,4 +9,25 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
+	listint *new_node;
+	listint *ptr = *head;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node != NULL)
+	{
+		new_node->n = n;
+		new_node->next = NULL;
+	}
+	else
+		return (NULL);
+	if (ptr != NULL)
+	{
+		while (ptr->next != NULL)
+			ptr = ptr->next;
+		ptr->next = new_node;
+	}
+	else
+		*head = new_node;
+	return (new_node);
+}
 	
