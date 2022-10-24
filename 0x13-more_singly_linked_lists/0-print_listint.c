e229caa5ee678d46dc4608f0d85bcadbe0ee5d80
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-10-24 11:59:12 +0200

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,4 +8,17 @@
  
  size_t print_listint(const listint_t *h)
  {
+  const listint *ptr = h;
+		size_t count = 0;
+		
+		while (ptr != NULL)
+		{
+			if (ptr->str != NULL)
+				printf("[%d]%s\n", ptr->str, ptr->len);
+		}
+		return (count);
+	}
+}
+  
+  
  
