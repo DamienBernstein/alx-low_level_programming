134591155925dad84a8d32489a08a1aa00854306
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-10-24 18:46:33 +0200

    Update 2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,10 +10,10 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	list_t *new_node;
-	
+
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
-		return(NULL);
+		return (NULL);
 
 	new_node->n = n;
 	new_node->next = *head;
@@ -22,7 +22,3 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	return (new_node);
 }
-	
-	
-	
-		
