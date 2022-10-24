2ce3cd7bb358eee46c639dfd1998c16ea4afd1f1
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-10-24 18:23:15 +0200

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,18 +5,17 @@
  * @h: header to pointer
  * Return: number of nodes
  */
- 
- size_t print_listint(const listint_t *h)
- {
-  const listint *ptr = h;
-		size_t count = 0;
 
-		while (ptr != NULL)
-		{
-				printf("%d\n", ptr->len);
-			count += 1;
-			ptr = ptr->next;
-		}
-		return (count);
-	}
+size_t print_listint(const listint_t *h)
+{
+size_t cont = 0;
+
+while (h != NULL)
+{
+printf("%i\n", h->n);
+h = h->next;
+	cont++;
+}
+return (cont);
+}
 }
