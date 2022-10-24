d95ef781d79ce84bb24c03cb1d3db7ea7db2cbef
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-10-24 14:18:29 +0200

    Update 6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -3,10 +3,21 @@
 /**
  * pop_listint - deletes the head node of a listint_t linked list, and returns the head node’s data (n).
  * @h: head of list
- * Return: returns the head node’s data (n).
+ * Return: the removed nodes data.
  */
 
 int pop_listint(listint_t **head)
 {
-	
-	
+	listint *node;
+	int data;
+
+	if (*head == NULL)
+	return (0);
+
+		node = *head;
+		data = node->n;
+		free(node);
+
+	*head = (*head)->next;
+	return (data);
+}
