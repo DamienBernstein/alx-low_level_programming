f7e0c5734ddd2f000a3b374cfb62dc02115d3b2a
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-10-25 09:07:34 +0200

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes the node at index of a listint_t linked list.
+ * delete_nodeint_at_index - deletes the node at
+ * index of a listint_t linked list.
  * @head: head of list
  * @index: is the index of the node that should be deleted
  * Return: 1 if it succeeded, -1 if it failed
@@ -9,32 +10,32 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *temp;
-	listint_t *rmovenode;
-	unsigned int i;
+listint_t *temp;
+listint_t *rmovenode;
+unsigned int i;
 
-	i = 0;
-	temp = *head;
-	if (*head == NULL)
-		return (-1);
-	if (index == 0)
-	{
-		*head = (*head)->next;
-		free(temp);
-	}
-	else
-	{
-		while (i < index - 1)
-		{
-		temp = temp->next;
-		  if (temp == NULL)
-		   return (-1);
-			i++;
-		}
-		rmovenode = temp;
-		rmovenode = rmovenode->next;
-		temp->next = rmovenode->next;
-		free(rmovenode);
-	}
-	return (1);
+i = 0;
+temp = *head;
+if (*head == NULL)
+return (-1);
+if (index == 0)
+{
+*head = (*head)->next;
+free(temp);
+}
+else
+{
+while (i < index - 1)
+{
+temp = temp->next;
+if (temp == NULL)
+return (-1);
+i++;
+}
+rmovenode = temp;
+rmovenode = rmovenode->next;
+temp->next = rmovenode->next;
+free(rmovenode);
+}
+return (1);
 }
