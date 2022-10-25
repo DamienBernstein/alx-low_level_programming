0e0c31f3d03109b96ccd93ed85e42e3ab2cb6141
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-10-25 10:22:15 +0200

    Create 102-free_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees a listint_t list.
+ * @h: header to list
+ * Return: the size of the list that was free’d
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
