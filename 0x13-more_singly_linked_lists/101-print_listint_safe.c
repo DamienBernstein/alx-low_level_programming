aed447588c97f1d298ca102f475171df7e123526
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-10-25 10:16:59 +0200

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,22 +1,22 @@
 #include lists.h"
 
 /**
- * print_listint_safe - prints a listint_t linked list.
- * @head: head of list
- *
- * Return: the number of nodes in the list
- */
+* print_listint_safe - prints a listint_t linked list.
+* @head: head of list
+*
+* Return: the number of nodes in the list
+*/
 
 size_t print_listint_safe(const listint_t *head)
 {
 	#include "lists.h"
 
 /**
- * free_listp - frees a linked list
- * @head: head of a list.
- *
- * Return: no return.
- */
+* free_listp - frees a linked list
+* @head: head of a list.
+*
+* Return: no return.
+*/
 void free_listp(listp_t **head)
 {
 	listp_t *temp;
@@ -35,11 +35,10 @@ void free_listp(listp_t **head)
 }
 
 /**
- * print_listint_safe - prints a linked list.
- * @head: head of a list.
- *
- * Return: number of nodes in the list.
- */
+* print_listint_safe - prints a linked list.
+* @head: head of a list.
+* Return: number of nodes in the list.
+*/
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t nnodes = 0;
@@ -64,13 +63,13 @@ size_t print_listint_safe(const listint_t *head)
 			add = add->next;
 			if (head == add->p)
 			{
-				printf("-> [%p] %d\n", (void *)head, head->n);
+				printf("->[%p]%d\n", (void *)head, head->n);
 				free_listp(&hptr);
 				return (nnodes);
 			}
 		}
 
-		printf("[%p] %d\n", (void *)head, head->n);
+		printf("[%p]%d\n", (void *)head, head->n);
 		head = head->next;
 		nnodes++;
 	}
