4963d6a7991f97523bdbab9fabdb35044459e149
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-10-27 15:47:34 +0200

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,12 +11,9 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (n == NULL)
-		return (-1);
-	if (index > (sizeof(unsigned long int) * 8) - 1))
+	if (n == NULL || (index > (sizeof(unsigned long int) * 8) - 1))
 		return (-1);
 
 	*n |= (1 << index);
 	return (1);
 }
-	
