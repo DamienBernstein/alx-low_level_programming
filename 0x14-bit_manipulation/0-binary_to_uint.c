6c72990719b3711ac4618e6fddaebf470aa161e8
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-10-27 15:13:33 +0200

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,63 +1,36 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-/**
- * _pow_recursion - function that returns the value of x
- * raised to the power of y
- * @x: base number
- * @y: pow number
- * Return: int
- */
-
-int _pow_recursion(int x, int y)
-{
-	if (y < 0)
-		return (-1);
-	if (y == 0)
-		return (1);
-	return (x * _pow_recursion(x, y - 1));
-}
 
 /**
- * _strlen - function that returns the length of a string.
- *
- * @s: pointer to an string
- * Return: int
+ *binary_to_uint - Write a function that converts a binary
+ *number to an unsigned int.
+ *@b: Is a number in binary
+ *Return: Binary number
  */
-
-int _strlen(const char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		i += 1;
-	}
-	return (i);
-}
-
-/**
- * binary_to_uint - function that converts a binary number to an unsigned int
- * @b: pointing to a string of 0 and 1 chars
- * Return: the converted number, or 0 if
- * there is one or more chars in the string b that is not 0 or 1
- * b is NULL
- */
-
 unsigned int binary_to_uint(const char *b)
 {
-	int len, exp = 0;
-	unsigned int res = 0;
+unsigned int number = 0;
+int len = 0, base = 1, i;
+char bin;
 
-	if (b == NULL)
+if (!b)
+	return (0);
+
+while (b[len] != '\0')
+	len++;
+
+for (i = len - 1; i >= 0; i--)
+{
+	if (b[i] != '0' && b[i] != '1')
 		return (0);
-	len = _strlen(b);
-	while (len-- && len >= 0)
+
+	bin = b[i];
+	if (bin == '1')
 	{
-		if (b[len] == '1')
-			res += _pow_recursion(2, exp);
-		else if (b[len] != '0')
-			return (0);
-		exp++;
+		number += base;
 	}
-	return (res);
+	base = base * 2;
+}
+return (number);
+}
