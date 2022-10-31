7882431f82870fcdc916a557c12f77e27053831f
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-10-31 14:57:48 +0200

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,11 +2,40 @@
 
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output.
- * @filename: filename
+ * @filename: file to read 
  * @letters: letters
  * Return: the actual number of letters it could read and print
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd, fd_read, fd_wite;
+	char *buff;
+
+	if (filename == NULL)
+		return (0);
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
+	fd = open(filename, O_RDWR);
+	if (fd == -1)
+	{
+		free(buff);
+		return (0);
+	}
+	fd_read = read(fd, buff, letters);
+	if (fd_read == -1)
+		return (0);
+	fd_write = write(STDOUT_FILENO, buff, fd_read);
+	if (fd_write == -1)
+	{
+		free(buff);
+		return (0);
+	}
+	if (fd_read != fd_write)
+		return (0);
+	free(buff);
+	close(fd);
+	return (fd_write);
+}
 	
