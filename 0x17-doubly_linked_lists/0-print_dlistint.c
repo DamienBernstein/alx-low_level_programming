20c659d223f4c647ee208b7e68bfd050925e23a1
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-12-08 11:46:47 +0200

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -5,8 +5,8 @@
 
 /**
  * print_dlistint-prints all the elements of a dlistint_t list.
- * @h-head
- * Return: the number of nodes
+ * @h: head
+ * return: the number of nodes
  */
 
 size_t print_dlistint(const dlistint_t *h)
