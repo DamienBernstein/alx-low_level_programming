59637d6913a1886b44fe6f00a92dd46d04c6ff79
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-12-08 11:45:26 +0200

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,13 +11,13 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-  size_t cont = 0;
-  
-  while (h != NULL)
-  {
-    printf("%d\n", h->n);
-    h = h->next;
-    cont++;
-  }
-  return (cont)
+size_t cont = 0;
+
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+cont++;
+}
+return (cont)
 }
